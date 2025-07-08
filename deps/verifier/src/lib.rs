9a08e2bcab7e208bba389c7b6cfb7dda1f64e3e8
Pradipta Banerjee <pradipta.banerjee@gmail.com>
2025-07-08 08:29:23 -0400

    Add TPM verifier
    Signed-off-by: Pradipta Banerjee <pradipta.banerjee@gmail.com>

--- deps/verifier/src/lib.rs
+++ deps/verifier/src/lib.rs
@@ -41,6 +41,9 @@ pub mod se;
 ))]
 pub mod intel_dcap;
 
+#[cfg(feature = "tpm-verifier")]
+pub mod tpm;
+
 pub fn to_verifier(tee: &Tee) -> Result<Box<dyn Verifier + Send + Sync>> {
     match tee {
         Tee::Sev => todo!(),
@@ -125,6 +128,18 @@ pub fn to_verifier(tee: &Tee) -> Result<Box<dyn Verifier + Send + Sync>> {
                 }
             }
         }
+
+        Tee::Tpm => {
+            cfg_if::cfg_if! {
+                if #[cfg(feature = "tpm-verifier")] {
+                    Ok(Box::<tpm::TpmVerifier>::default() as Box<dyn Verifier + Send + Sync>)
+                } else {
+                    bail!("feature `tpm-verifier` is not enabled for `verifier` crate.")
+                }
+            }
+        }
+
+        Tee::HygonDcu => todo!(),
     }
 }
 
